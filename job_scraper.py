c80d8063225684a07e4df5a693431443b1462b51
Laurie Lambot <36240376+llambot@users.noreply.github.com>
2026-05-22 11:20:47 -0600

    Update job_scraper.py
    3 days old jobs

--- job_scraper.py
+++ job_scraper.py
@@ -140,7 +140,7 @@ def scrape_all() -> list[dict]:
                 search_term=query_config["query"],
                 location=query_config["location"],
                 results_wanted=20,
-		hours_old=720,     # 30 days to catch more jobs
+		hours_old=72,     # 3 days to catch more jobs
 		country_indeed="USA",
 		job_type="fulltime",
             )
